a6654f32a6e03b1cc5c4e2005dcf60083bab6212
bitplorer <bitplorer@outlook.com>
2023-03-15 20:32:58 +0530

    renamed or deleted files

--- uidom/components/alpinejs/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022 uidom
-# 
-# This software is released under the MIT License.
-# https://opensource.org/licenses/MIT
-
-
-from .slot_element import *
-from .web_component_slot import *
