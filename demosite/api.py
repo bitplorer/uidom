0249558312f15de1a5b457ceacf6393e1aa6595e
bitplorer <bitplorer@outlook.com>
2023-03-16 23:06:27 +0530

    update: api settings

--- demosite/api.py
+++ demosite/api.py
@@ -5,24 +5,40 @@
 
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 
-from demosite.document import document
+from uidom.routing import HTMLRoute, StreamingRoute
 
-api = FastAPI(debug=True)
-if document.webassets is not None:
-    api.mount(
-        "/css",
-        StaticFiles(directory=document.webassets.static.css, check_dir=False),
-        name="css",
-    )
-    api.mount(
-        "/js",
-        StaticFiles(directory=document.webassets.static.js, check_dir=False),
-        name="js",
-    )
-    api.mount(
-        "/image",
-        StaticFiles(directory=document.webassets.static.images, check_dir=False),
-        name="image",
-    )
+from . import settings
+from .pages.card import card_router
+from .pages.chart import chart_router
+from .pages.header import header_router
+
+api = FastAPI(debug=settings.DEBUG, default_response_class=HTMLResponse, title="Demosite")
+api.router.route_class = StreamingRoute 
+api.include_router(header_router)
+api.include_router(chart_router)
+api.include_router(card_router)
+
+if settings.DEBUG:
+    # adding browser reloading 
+    api.add_websocket_route(settings.HOT_RELOAD_URL, route=settings.hot_reload, name=settings.HOT_RELOAD_URL_NAME)
+    api.add_event_handler("startup", settings.hot_reload.startup)
+    api.add_event_handler("shutdown", settings.hot_reload.shutdown)
+
+api.mount(
+    "/css",
+    StaticFiles(directory=settings.webassets.static.css, check_dir=False),
+    name="css",
+)
+api.mount(
+    "/js",
+    StaticFiles(directory=settings.webassets.static.js, check_dir=False),
+    name="js",
+)
+api.mount(
+    "/image",
+    StaticFiles(directory=settings.webassets.static.images, check_dir=False),
+    name="image",
+)
