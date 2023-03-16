b815137bfae4f7e6cd93ea674f91bcd140f609da
bitplorer <bitplorer@outlook.com>
2023-03-16 23:01:32 +0530

    add: routes

---
+++ demosite/routes.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022 uidom
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+from demosite.index import api
+from demosite.pages.counter import api
+from demosite.pages.datepicker import api
+from demosite.pages.search import api
