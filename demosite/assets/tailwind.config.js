c08f60f62b86f82eddb1a2d5be12a908174dbf06
bitplorer <bitplorer@outlook.com>
2023-04-12 03:04:25 +0530

    Updated: demosite files

---
+++ demosite/assets/tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    mode: "jit",
+    darkMode: "class",
+    content: {
+        files:[
+        "../../demosite/*.{html,py}",
+        "../../demosite/**/*.{html,py}",
+        "../../demosite/**/**/*.{html,py}",
+        ]
+        },
+    plugins: [
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/line-clamp'),
+        require('@tailwindcss/typography'),
+        require('tailwindcss/colors'),
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                roboto: ["'Roboto'"],
+                cursive: ["'Style Script'"],
+                oswald: ["'Oswald'"],
+                teko: ["'Teko'"],
+                cinzel: ["'Cinzel Decorative'"],
+                montserrat: ["'Montserrat'"],
+            },
+            minHeight: (theme) => ({
+                ...theme('spacing'),
+              })
+    }
+    }
+    
+    }
