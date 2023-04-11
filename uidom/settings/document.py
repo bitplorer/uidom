1d9247ebdcef109cb622c1a225df535b83e35ece
bitplorer <bitplorer@outlook.com>
2023-04-12 03:09:09 +0530

    Updated: WebAssets class inheritance

--- uidom/settings/document.py
+++ uidom/settings/document.py
@@ -1,8 +1,9 @@
 # Copyright (c) 2022 uidom
-# 
+#
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
+import logging
 import os
 import sys
 import typing as T
@@ -14,6 +15,9 @@ from uidom.dom.src import ext
 from uidom.settings.paths import make_paths
 
 __all__ = ["UiDOM", "WebAssets", "FileSettings"]
+
+file_logger = logging.getLogger(__name__)
+file_logger.addHandler(logging.StreamHandler())
 
 
 @dataclass
@@ -33,7 +37,7 @@ class UiDOM(object):
             body=body,
             common_head=self.head,
             common_body=self.body,
-            **kwargs
+            **kwargs,
         )
 
 
@@ -63,13 +67,13 @@ class FileSettings(object):
         #     raise ValueError(f"BASE_DIR, BASE_PATH values required")
         if isinstance(self.BASE_DIR, str):
             if os.path.isfile(self.BASE_DIR):
+                # like when __file__ is passed in the BASE_DIR param
                 self.BASE_DIR = Path(self.BASE_DIR).parent.resolve()
             else:
                 self.BASE_DIR = Path(self.BASE_DIR).resolve()
 
 
 class DirConfig(object):
-
     def __init__(self, file_settings: FileSettings):
         subdir = (
             file_settings.SUB_DIR
@@ -78,15 +82,24 @@ class DirConfig(object):
         )
 
         root_dir = (
-            os.path.join(file_settings.BASE_DIR, subdir)
-            if os.path.isdir(file_settings.BASE_DIR)
-            else os.path.join(file_settings.BASE_DIR.replace(".", os.path.sep), subdir)
-            if not os.path.isfile(file_settings.BASE_DIR)
-            else os.path.join(os.path.dirname(file_settings.BASE_DIR), subdir)
-            if file_settings.BASE_DIR != "__main__"
-            else os.path.join(os.path.dirname(sys.modules["__main__"].__file__), subdir)
-        ) if isinstance(file_settings.BASE_DIR, str) \
-            else os.path.join(file_settings.BASE_DIR, subdir) if any(subdir) else file_settings.BASE_DIR
+            (
+                os.path.join(file_settings.BASE_DIR, subdir)
+                if os.path.isdir(file_settings.BASE_DIR)
+                else os.path.join(
+                    file_settings.BASE_DIR.replace(".", os.path.sep), subdir
+                )
+                if not os.path.isfile(file_settings.BASE_DIR)
+                else os.path.join(os.path.dirname(file_settings.BASE_DIR), subdir)
+                if file_settings.BASE_DIR != "__main__"
+                else os.path.join(
+                    os.path.dirname(sys.modules["__main__"].__file__), subdir
+                )
+            )
+            if isinstance(file_settings.BASE_DIR, str)
+            else os.path.join(file_settings.BASE_DIR, subdir)
+            if any(subdir)
+            else file_settings.BASE_DIR
+        )
 
         self.ROOT_DIR = Path(
             root_dir if any([root_dir]) else os.path.abspath(f".{os.path.sep}")
@@ -126,9 +139,11 @@ class DirConfig(object):
         return next(self)
 
     def dict(self):
-        return {item: self.__dict__[item]
-                for item in self.__dict__
-                if "ROOT" or "DIR" in item}
+        return {
+            item: self.__dict__[item]
+            for item in self.__dict__
+            if "ROOT" or "DIR" in item
+        }
 
     def __str__(self):
         return str(list(self))
@@ -137,27 +152,61 @@ class DirConfig(object):
 class UrlConfig(object):
     def __init__(self, dir_config: DirConfig):
         self.ROOT_URL = dir_config.ROOT_DIR
-        self.TEMPLATE_URL = Path(os.path.sep) / dir_config.TEMPLATE_DIR.relative_to(dir_config.ROOT_DIR)
-        self.STATIC_URL = Path(os.path.sep) / dir_config.STATIC_DIR.relative_to(dir_config.ROOT_DIR)
-        self.UPLOAD_URL = Path(os.path.sep) / dir_config.UPLOAD_DIR.relative_to(dir_config.ROOT_DIR)
+        self.TEMPLATE_URL = Path(os.path.sep) / dir_config.TEMPLATE_DIR.relative_to(
+            dir_config.ROOT_DIR
+        )
+        self.STATIC_URL = Path(os.path.sep) / dir_config.STATIC_DIR.relative_to(
+            dir_config.ROOT_DIR
+        )
+        self.UPLOAD_URL = Path(os.path.sep) / dir_config.UPLOAD_DIR.relative_to(
+            dir_config.ROOT_DIR
+        )
 
-        self.STATIC_MEDIA_URL = Path(os.path.sep) / dir_config.STATIC_MEDIA_DIR.relative_to(dir_config.STATIC_DIR)
-        self.STATIC_FILE_URL = Path(os.path.sep) / dir_config.STATIC_FILE_DIR.relative_to(dir_config.STATIC_DIR)
+        self.STATIC_MEDIA_URL = Path(
+            os.path.sep
+        ) / dir_config.STATIC_MEDIA_DIR.relative_to(dir_config.STATIC_DIR)
+        self.STATIC_FILE_URL = Path(
+            os.path.sep
+        ) / dir_config.STATIC_FILE_DIR.relative_to(dir_config.STATIC_DIR)
 
-        self.STATIC_CSS_URL = Path(os.path.sep) / dir_config.STATIC_CSS_DIR.relative_to(dir_config.STATIC_FILE_DIR)
-        self.STATIC_JS_URL = Path(os.path.sep) / dir_config.STATIC_JS_DIR.relative_to(dir_config.STATIC_FILE_DIR)
-        self.STATIC_FONT_URL = Path(os.path.sep) / dir_config.STATIC_FONT_DIR.relative_to(dir_config.STATIC_FILE_DIR)
-        self.STATIC_IMAGE_URL = Path(os.path.sep) / dir_config.STATIC_IMAGE_DIR.relative_to(dir_config.STATIC_MEDIA_DIR)
-        self.STATIC_AUDIO_URL = Path(os.path.sep) / dir_config.STATIC_AUDIO_DIR.relative_to(dir_config.STATIC_MEDIA_DIR)
-        self.STATIC_VIDEO_URL = Path(os.path.sep) / dir_config.STATIC_VIDEO_DIR.relative_to(dir_config.STATIC_MEDIA_DIR)
+        self.STATIC_CSS_URL = Path(os.path.sep) / dir_config.STATIC_CSS_DIR.relative_to(
+            dir_config.STATIC_FILE_DIR
+        )
+        self.STATIC_JS_URL = Path(os.path.sep) / dir_config.STATIC_JS_DIR.relative_to(
+            dir_config.STATIC_FILE_DIR
+        )
+        self.STATIC_FONT_URL = Path(
+            os.path.sep
+        ) / dir_config.STATIC_FONT_DIR.relative_to(dir_config.STATIC_FILE_DIR)
+        self.STATIC_IMAGE_URL = Path(
+            os.path.sep
+        ) / dir_config.STATIC_IMAGE_DIR.relative_to(dir_config.STATIC_MEDIA_DIR)
+        self.STATIC_AUDIO_URL = Path(
+            os.path.sep
+        ) / dir_config.STATIC_AUDIO_DIR.relative_to(dir_config.STATIC_MEDIA_DIR)
+        self.STATIC_VIDEO_URL = Path(
+            os.path.sep
+        ) / dir_config.STATIC_VIDEO_DIR.relative_to(dir_config.STATIC_MEDIA_DIR)
 
-        self.UPLOAD_MEDIA_URL = Path(os.path.sep) / dir_config.UPLOAD_MEDIA_DIR.relative_to(dir_config.UPLOAD_DIR)
-        self.UPLOAD_IMAGE_URL = Path(os.path.sep) / dir_config.UPLOAD_IMAGE_DIR.relative_to(dir_config.UPLOAD_MEDIA_DIR)
-        self.UPLOAD_AUDIO_URL = Path(os.path.sep) / dir_config.UPLOAD_AUDIO_DIR.relative_to(dir_config.UPLOAD_MEDIA_DIR)
-        self.UPLOAD_VIDEO_URL = Path(os.path.sep) / dir_config.UPLOAD_VIDEO_DIR.relative_to(dir_config.UPLOAD_MEDIA_DIR)
-        self.UPLOAD_FILE_URL = Path(os.path.sep) / dir_config.UPLOAD_FILE_DIR.relative_to(dir_config.UPLOAD_DIR)
+        self.UPLOAD_MEDIA_URL = Path(
+            os.path.sep
+        ) / dir_config.UPLOAD_MEDIA_DIR.relative_to(dir_config.UPLOAD_DIR)
+        self.UPLOAD_IMAGE_URL = Path(
+            os.path.sep
+        ) / dir_config.UPLOAD_IMAGE_DIR.relative_to(dir_config.UPLOAD_MEDIA_DIR)
+        self.UPLOAD_AUDIO_URL = Path(
+            os.path.sep
+        ) / dir_config.UPLOAD_AUDIO_DIR.relative_to(dir_config.UPLOAD_MEDIA_DIR)
+        self.UPLOAD_VIDEO_URL = Path(
+            os.path.sep
+        ) / dir_config.UPLOAD_VIDEO_DIR.relative_to(dir_config.UPLOAD_MEDIA_DIR)
+        self.UPLOAD_FILE_URL = Path(
+            os.path.sep
+        ) / dir_config.UPLOAD_FILE_DIR.relative_to(dir_config.UPLOAD_DIR)
 
-        self.SQL_DATABASE_URL = Path(os.path.sep) / dir_config.SQL_DATABASE_DIR.relative_to(dir_config.ROOT_DIR)
+        self.SQL_DATABASE_URL = Path(
+            os.path.sep
+        ) / dir_config.SQL_DATABASE_DIR.relative_to(dir_config.ROOT_DIR)
 
     def __iter__(self):
         for item in self.__dict__:
@@ -168,17 +217,15 @@ class UrlConfig(object):
         return next(self)
 
     def dict(self):
-        return {item: self.__dict__[item]
-                for item in self.__dict__
-                if "URL" in item}
+        return {item: self.__dict__[item] for item in self.__dict__ if "URL" in item}
 
     def __str__(self):
         return str(list(self))
 
 
 class DatabaseFile(object):
-
     def __init__(self, dir_config: DirConfig):
+        self.dir: Path = dir_config.DATABASE_DIR
         self.sql: Path = dir_config.SQL_DATABASE_DIR
         self.nosql: Path = dir_config.NOSQL_DATABASE_DIR
 
@@ -211,9 +258,8 @@ class TemplateFile(object):
         self.dir: Path = dir_config.TEMPLATE_DIR
 
 
-class WebAssets(object):
+class WebAssets(DirConfig):
     files: FileSettings
-    directory: DirConfig
     url: UrlConfig
     template: TemplateFile
     upload: UploadedFile
@@ -221,26 +267,25 @@ class WebAssets(object):
     database: DatabaseFile
 
     def __init__(self, file_settings: FileSettings):
-
+        super().__init__(file_settings=file_settings)
         # basic settings
-        self.directory = DirConfig(file_settings=file_settings)
-        self.url = UrlConfig(dir_config=self.directory)
+        self.dir = self.ROOT_DIR
+        # url settings
+        self.url = UrlConfig(dir_config=self)
 
         # files
-        self.template = TemplateFile(dir_config=self.directory)
-        self.upload = UploadedFile(dir_config=self.directory)
-        self.static = StaticFile(dir_config=self.directory)
-        self.database = DatabaseFile(dir_config=self.directory)
-        files_created = make_paths(self.directory)
+        self.template = TemplateFile(dir_config=self)
+        self.upload = UploadedFile(dir_config=self)
+        self.static = StaticFile(dir_config=self)
+        self.database = DatabaseFile(dir_config=self)
+        files_created = make_paths(self)
         if files_created:
-            print("files created :: ")
+            file_logger.info("files created :: ")
             for file in files_created:
-                print("\tfile: ", file)
+                file_logger.info("\n\tfile: ", file)
 
-
-if __name__ == '__main__':
-    from pathlib import Path
-
-    from uidom.dom import div, meta, title
-    doc = UiDOM()
-    print(doc("hahah"))
+    def __dir__(self) -> T.Iterable[str]:
+        return sorted(
+            iter(self.__dict__),
+            key=lambda k: (~k.startswith("__"), k[0].lower(), k[0]),
+        )
