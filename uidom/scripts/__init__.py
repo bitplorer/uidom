f4c7ee9244a17aa7f7c08eb016c80a43fe16e230
bitplorer <bitplorer@outlook.com>
2023-04-12 17:47:20 +0530

    added: x_component_js class

--- uidom/scripts/__init__.py
+++ uidom/scripts/__init__.py
@@ -6,18 +6,26 @@
 from functools import lru_cache
 from pathlib import Path
 
-from uidom.dom import raw, script
+from uidom.dom import raw
+from uidom.dom.src.component import Component
 
 __all__ = ["x_component_js_text"]
-
-X_COMPONENT_SCRIPT_FILE = Path(__file__).parent / "xcomponent.js"
-assert X_COMPONENT_SCRIPT_FILE.exists()
 
 
 @lru_cache
 def x_component_js_text():
+    X_COMPONENT_SCRIPT_FILE = Path(__file__).parent / "xcomponent.js"
+    if not X_COMPONENT_SCRIPT_FILE.exists():
+        raise FileNotFoundError(f"{X_COMPONENT_SCRIPT_FILE=} doesn't exists")
     return X_COMPONENT_SCRIPT_FILE.read_text()
 
 
+class x_component_js(Component):
+    file_extension = ".js"
+
+    def render(self):
+        return raw(x_component_js_text())
+
+
 if __name__ == "__main__":
-    print(x_component_js_text)
+    print(x_component_js())
