44a14a14039667fe29c03c11b8cfee8cf26d032d
bitplorer <bitplorer@outlook.com>
2023-04-12 17:19:20 +0530

    reformatting

--- uidom/slots/custom_element_slot.py
+++ uidom/slots/custom_element_slot.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2022 uidom
-# 
+#
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
@@ -9,10 +9,7 @@ from dataclasses import dataclass, field
 from uidom.dom.htmlelement import *
 from uidom.dom.src.htmltags import *
 
-__all__ = [
-    "SlotElement",
-    "x_slot"
-]
+__all__ = ["SlotElement", "x_slot"]
 
 
 @dataclass
@@ -32,12 +29,20 @@ class SlotElement(CustomElement):
         """
         return template(
             div(
-                template(link(x_bind_href="ss", rel="stylesheet", type="text/css"), x_for="ss in css"),
                 template(
-                    div(slot(x_bind_name="name"), x_bind_part="name", x_bind_class="classes[name]",
-                        exportparts="*"),
-                    x_for="name in slotnames", tabindex=0,
-                    x_if=" !!slotnames && slotnames.length && slotnames[0] !== '' "
+                    link(x_bind_href="ss", rel="stylesheet", type="text/css"),
+                    x_for="ss in css",
+                ),
+                template(
+                    div(
+                        slot(x_bind_name="name"),
+                        x_bind_part="name",
+                        x_bind_class="classes[name]",
+                        exportparts="*",
+                    ),
+                    x_for="name in slotnames",
+                    tabindex=0,
+                    x_if=" !!slotnames && slotnames.length && slotnames[0] !== '' ",
                 ),
                 template(
                     slot(),
@@ -47,17 +52,18 @@ class SlotElement(CustomElement):
                 x_bind_class="classes[`${container_part}`]",
                 x_bind_part="container_part",
                 exportparts="*",
-                x_data="{...slots(), ...$el.parentElement.data()}", x_cloak=None,
+                x_data="{...slots(), ...$el.parentElement.data()}",
+                x_cloak=None,
             ),
             script(
-                '''function slots(){
+                """function slots(){
                 return {
                     slotnames: [''],
                     classes:{}, 
                     css:[''] , 
                     container_part: ''
                         }
-                    }'''
+                    }"""
             ),
             x_component=tag_name,
         )
@@ -65,5 +71,5 @@ class SlotElement(CustomElement):
 
 x_slot = SlotElement(tag_name="slot")
 
-if __name__ == '__main__':
-    print(x_slot())
+if __name__ == "__main__":
+    print(x_slot.get(x_data=None))
