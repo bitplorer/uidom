9e18b91433446cce702ab27aea761500487f15bb
bitplorer <bitplorer@outlook.com>
2023-05-01 01:12:34 +0530

    contrib: placeholder for future implementations

---
+++ uidom/contrib/__init__.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2023 UiDOM
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+
+from importlib import import_module, resources
+
+PLUGINS = dict()
+
+
+def register_plugin(func):
+    """Decorator to register plug-ins"""
+    name = func.__name__
+    PLUGINS[name] = func
+    return func
+
+
+def __getattr__(name):
+    """Return a named plugin"""
+    try:
+        return PLUGINS[name]
+    except KeyError:
+        _import_plugins()
+        if name in PLUGINS:
+            return PLUGINS[name]
+        else:
+            raise AttributeError(
+                f"module {__name__!r} has no attribute {name!r}"
+            ) from None
+
+
+def __dir__():
+    """List available plug-ins"""
+    _import_plugins()
+    return list(PLUGINS.keys())
+
+
+def _import_plugins():
+    """Import all resources to register plug-ins"""
+    for name in resources.contents(__name__):
+        if name.endswith(".py"):
+            import_module(f"{__name__}.{name[:-3]}")
