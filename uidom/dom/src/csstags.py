8545be1a68fa88141d1ebf25e6357f1fde7d462e
bitplorer <bitplorer@outlook.com>
2023-04-30 18:44:38 +0530

    WIP

--- uidom/dom/src/csstags.py
+++ uidom/dom/src/csstags.py
@@ -1,17 +1,12 @@
 # Copyright (c) 2022 uidom
-# 
+#
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
 
 from uidom.dom.src.main import extension
 
-__all__ = [
-    "CSSClass",
-    "CSSId",
-    "CSS",
-    "CSSProperty"
-]
+__all__ = ["CSSClass", "CSSId", "CSS", "CSSProperty"]
 
 
 class CSS(extension.StyleTags):
@@ -29,59 +24,57 @@ class CSSId(extension.StyleTags):
 
 
 class CSSProperty(extension.StyleTags):
-
     def __set_name__(self, owner, name):
+        self.is_inline = getattr(owner, "is_inline", False)
         self._id = f"{owner.__name__}.{name}".replace(".", "-").replace("_", "-")
         self._class = name.replace("_", "-")
-        self._apply = f"{owner.__name__}::part({name})".replace("_", "-")
+        self._apply_part = f"{owner.__name__}::part({name})".replace("_", "-")
+        self._apply = f"{owner.__name__}".replace("_", "-")
 
         class Class(CSSClass):
             tagname = self._class
+            is_inline = self.is_inline
 
         class Id(CSSId):
             tagname = self._id.lower()
+            is_inline = self.is_inline
 
         class CSS(extension.StyleTags):
             tagname = name.replace("_", "-")
+            is_inline = self.is_inline
 
         class Apply(extension.StyleTags):
             tagname = self._apply.lower()
             is_apply = True
-            left_delimiter = "{@"
+            is_inline = self.is_inline
 
-        self.css_class = Class
-        self.css_id = Id
+        self.cls = Class
+        self.id = Id
         self.css = CSS
-        self.css_apply = Apply
+        self.apply = Apply
 
 
-if __name__ == '__main__':
-    # from myst_parser import parse_html
+if __name__ == "__main__":
 
-    class Order(object):
+    class div(extension.Tags):
+        pass
+
+    class Order(extension.Tags):
+        render_tag = False
         product_name = CSSProperty()
         product_category = CSSProperty()
         product_id = CSSProperty()
 
-
-    order = Order()
-    products = order.product_name.css_id(background_color='red', width="12%")
-    category = order.product_category.css_class(color="#23cfff")
-    csss = order.product_id.css_apply(apply="bg-rose-500")
-    id = order.product_id.css_id()
-
-
-    # ast = parse_html.tokenize_html(html(style(products, category)).render())
-    # print(list(ast.walk()))
-
-    class product(CSSClass):
-        pass
-
-
-    cs = products
-    # cs.attributes.update(padding="10px")
-    cs["boxsize"] = "border-box"
-    cs["background-color"] = "red"
-    print(cs)
+    order = Order(div("hello"))
+    prod_name = Order.product_name.id(background_color="red", width="12%")
+    category = Order.product_category.cls(color="#23cfff")
+    prod_id = Order.product_id.apply(apply="bg-rose-500 text-rose-400")
+    print(order)
+    # products.attributes.update(padding="10px")
+    prod_name["boxsize"] = "border-box"
+    prod_name["background-color"] = "red"
+    # print(prod_name)
+    # print(order.product_name.css(textdecoration="none"))
     print(category)
-    print(csss.__render__(pretty=False))
+    prod_id.add(prod_name)
+    print(prod_id)
