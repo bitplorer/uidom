ff020bb99a9214ccb4ba44e80a283aa00d0d7195
bitplorer <bitplorer@outlook.com>
2023-05-04 22:24:27 +0530

    updated: Directory Classes and Structures

--- uidom/settings/document.py
+++ uidom/settings/document.py
@@ -7,14 +7,15 @@ import logging
 import os
 import sys
 import typing as T
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
+from pprint import pformat
 
 from uidom.dom.htmldocument import HtmlDocument
 from uidom.dom.src import ext
 from uidom.settings.paths import make_paths
 
-__all__ = ["UiDOM", "WebAssets", "FileSettings"]
+__all__ = ["UiDOM", "WebAssets", "Dir", "DirConfig"]
 
 
 # file_logger.addHandler(logging.StreamHandler())
@@ -24,7 +25,7 @@ __all__ = ["UiDOM", "WebAssets", "FileSettings"]
 class UiDOM(object):
     head: T.Optional[T.Union[ext.Tags, list[ext.Tags]]] = None
     body: T.Optional[T.Union[ext.Tags, list[ext.Tags]]] = None
-    ensure_csrf_token_in_meta: T.Union[bool, None] = False
+    ensure_csrf_token_in_meta: bool = field(default=False)
     webassets: T.Optional["WebAssets"] = None
 
     def __call__(self, *args, head=None, body=None, **kwargs) -> HtmlDocument:
@@ -41,96 +42,7 @@ class UiDOM(object):
         )
 
 
-@dataclass
-class FileSettings(object):
-    BASE_DIR: T.Union[str, Path]
-    SUB_DIR: T.Union[str, Path] = ""
-    TEMPLATE_PATH = "templates"
-    CACHE_PATH = "cache"
-    STATIC_PATH = "static"
-    DATABASE_PATH = "database"
-    UPLOAD_PATH = "upload"
-    MEDIA_PATH = "media"
-    FILE_PATH = "file"
-    CSS_PATH = "css"
-    JS_PATH = "js"
-    FONT_PATH = "font"
-    IMAGE_PATH = "image"
-    AUDIO_PATH = "audio"
-    VIDEO_PATH = "video"
-
-    SQL_PATH = "sql"
-    NOSQL_PATH = "no_sql"
-
-    def __post_init__(self):
-        # if not all([self.BASE_DIR, self.SUB_DIR]):
-        #     raise ValueError(f"BASE_DIR, BASE_PATH values required")
-        if isinstance(self.BASE_DIR, str):
-            if os.path.isfile(self.BASE_DIR):
-                # like when __file__ is passed in the BASE_DIR param
-                self.BASE_DIR = Path(self.BASE_DIR).parent.resolve()
-            else:
-                self.BASE_DIR = Path(self.BASE_DIR).resolve()
-
-
-class DirConfig(object):
-    def __init__(self, file_settings: FileSettings):
-        assert sys.modules["__main__"].__file__ is not None
-        subdir = (
-            file_settings.SUB_DIR
-            if not os.path.isfile(file_settings.SUB_DIR)
-            else os.path.dirname(file_settings.SUB_DIR)
-        )
-
-        root_dir = (
-            (
-                os.path.join(file_settings.BASE_DIR, subdir)
-                if os.path.isdir(file_settings.BASE_DIR)
-                else os.path.join(
-                    file_settings.BASE_DIR.replace(".", os.path.sep), subdir
-                )
-                if not os.path.isfile(file_settings.BASE_DIR)
-                else os.path.join(os.path.dirname(file_settings.BASE_DIR), subdir)
-                if file_settings.BASE_DIR != "__main__"
-                else os.path.join(
-                    os.path.dirname(sys.modules["__main__"].__file__), subdir
-                )
-            )
-            if isinstance(file_settings.BASE_DIR, str)
-            else os.path.join(file_settings.BASE_DIR, subdir)
-            if any(subdir)
-            else file_settings.BASE_DIR
-        )
-
-        self.ROOT_DIR = Path(
-            root_dir if any([root_dir]) else os.path.abspath(f".{os.path.sep}")
-        )
-        self.TEMPLATE_DIR = self.ROOT_DIR / file_settings.TEMPLATE_PATH
-        self.CACHE_DIR = self.ROOT_DIR / file_settings.CACHE_PATH
-        self.DATABASE_DIR = self.ROOT_DIR / file_settings.DATABASE_PATH
-        self.STATIC_DIR = self.ROOT_DIR / file_settings.STATIC_PATH
-        self.UPLOAD_DIR = self.ROOT_DIR / file_settings.UPLOAD_PATH
-
-        self.STATIC_MEDIA_DIR = self.STATIC_DIR / file_settings.MEDIA_PATH
-        self.STATIC_FILE_DIR = self.STATIC_DIR / file_settings.FILE_PATH
-
-        self.STATIC_CSS_DIR = self.STATIC_FILE_DIR / file_settings.CSS_PATH
-        self.STATIC_JS_DIR = self.STATIC_FILE_DIR / file_settings.JS_PATH
-        self.STATIC_FONT_DIR = self.STATIC_FILE_DIR / file_settings.FONT_PATH
-
-        self.STATIC_IMAGE_DIR = self.STATIC_MEDIA_DIR / file_settings.IMAGE_PATH
-        self.STATIC_AUDIO_DIR = self.STATIC_MEDIA_DIR / file_settings.AUDIO_PATH
-        self.STATIC_VIDEO_DIR = self.STATIC_MEDIA_DIR / file_settings.VIDEO_PATH
-
-        self.UPLOAD_MEDIA_DIR = self.UPLOAD_DIR / file_settings.MEDIA_PATH
-        self.UPLOAD_FILE_DIR = self.UPLOAD_DIR / file_settings.FILE_PATH
-        self.UPLOAD_IMAGE_DIR = self.UPLOAD_MEDIA_DIR / file_settings.IMAGE_PATH
-        self.UPLOAD_AUDIO_DIR = self.UPLOAD_MEDIA_DIR / file_settings.AUDIO_PATH
-        self.UPLOAD_VIDEO_DIR = self.UPLOAD_MEDIA_DIR / file_settings.VIDEO_PATH
-
-        self.SQL_DATABASE_DIR = self.DATABASE_DIR / file_settings.SQL_PATH
-        self.NOSQL_DATABASE_DIR = self.DATABASE_DIR / file_settings.NOSQL_PATH
-
+class DirType(object):
     def __iter__(self):
         for item in self.__dict__:
             if "ROOT" in item or "DIR" in item:
@@ -140,153 +52,212 @@ class DirConfig(object):
         return next(self)
 
     def dict(self):
-        return {
-            item: self.__dict__[item]
-            for item in self.__dict__
-            if "ROOT" or "DIR" in item
-        }
+        return {item: self.__dict__[item] for item in self.__dict__ if "DIR" in item}
 
     def __str__(self):
         return str(list(self))
 
 
-class UrlConfig(object):
-    def __init__(self, dir_config: DirConfig):
-        self.ROOT_URL = dir_config.ROOT_DIR
-        self.TEMPLATE_URL = Path(os.path.sep) / dir_config.TEMPLATE_DIR.relative_to(
-            dir_config.ROOT_DIR
+@dataclass
+class DirConfig(DirType):
+    base_dir: T.Union[str, Path]
+    sub_dir: T.Union[str, Path] = ""
+
+    def __post_init__(self):
+        # if not all([self.BASE_DIR, self.SUB_DIR]):
+        #     raise ValueError(f"BASE_DIR, BASE_PATH values required")
+        if isinstance(self.base_dir, str):
+            if os.path.isfile(self.base_dir):
+                # like when __file__ is passed in the BASE_DIR param
+                self.BASE_DIR = Path(self.base_dir).parent.resolve()
+            else:
+                self.BASE_DIR = Path(self.base_dir).resolve()
+
+        # self.SUB_DIR = self.sub_dir
+
+        subdir: T.Union[str, Path] = (
+            self.sub_dir
+            if not os.path.isfile(self.sub_dir)
+            else os.path.dirname(self.sub_dir)
         )
-        self.STATIC_URL = Path(os.path.sep) / dir_config.STATIC_DIR.relative_to(
-            dir_config.ROOT_DIR
+
+        base_dir: T.Union[str, Path] = self.BASE_DIR
+
+        if subdir:
+            if isinstance(self.BASE_DIR, str):
+                if self.BASE_DIR != "__main__":
+                    if not os.path.isfile(self.BASE_DIR):
+                        if os.path.isdir(self.BASE_DIR):
+                            base_dir = os.path.join(self.BASE_DIR, subdir)
+                        else:
+                            base_dir = os.path.join(
+                                self.BASE_DIR.replace(".", os.path.sep), subdir
+                            )
+                    else:
+                        base_dir = os.path.join(os.path.dirname(self.BASE_DIR), subdir)
+                else:
+                    folder = sys.modules["__main__"].__file__ or ""
+                    base_dir = (
+                        Path(folder).parent / subdir
+                        if folder
+                        else Path(folder) / subdir
+                    )
+            else:
+                base_dir = self.BASE_DIR / subdir
+
+        self.BASE_DIR = Path(
+            base_dir if any([base_dir]) else os.path.abspath(f".{os.path.sep}")
         )
-        self.UPLOAD_URL = Path(os.path.sep) / dir_config.UPLOAD_DIR.relative_to(
-            dir_config.ROOT_DIR
-        )
-
-        self.STATIC_MEDIA_URL = Path(
-            os.path.sep
-        ) / dir_config.STATIC_MEDIA_DIR.relative_to(dir_config.STATIC_DIR)
-        self.STATIC_FILE_URL = Path(
-            os.path.sep
-        ) / dir_config.STATIC_FILE_DIR.relative_to(dir_config.STATIC_DIR)
-
-        self.STATIC_CSS_URL = Path(os.path.sep) / dir_config.STATIC_CSS_DIR.relative_to(
-            dir_config.STATIC_FILE_DIR
-        )
-        self.STATIC_JS_URL = Path(os.path.sep) / dir_config.STATIC_JS_DIR.relative_to(
-            dir_config.STATIC_FILE_DIR
-        )
-        self.STATIC_FONT_URL = Path(
-            os.path.sep
-        ) / dir_config.STATIC_FONT_DIR.relative_to(dir_config.STATIC_FILE_DIR)
-        self.STATIC_IMAGE_URL = Path(
-            os.path.sep
-        ) / dir_config.STATIC_IMAGE_DIR.relative_to(dir_config.STATIC_MEDIA_DIR)
-        self.STATIC_AUDIO_URL = Path(
-            os.path.sep
-        ) / dir_config.STATIC_AUDIO_DIR.relative_to(dir_config.STATIC_MEDIA_DIR)
-        self.STATIC_VIDEO_URL = Path(
-            os.path.sep
-        ) / dir_config.STATIC_VIDEO_DIR.relative_to(dir_config.STATIC_MEDIA_DIR)
-
-        self.UPLOAD_MEDIA_URL = Path(
-            os.path.sep
-        ) / dir_config.UPLOAD_MEDIA_DIR.relative_to(dir_config.UPLOAD_DIR)
-        self.UPLOAD_IMAGE_URL = Path(
-            os.path.sep
-        ) / dir_config.UPLOAD_IMAGE_DIR.relative_to(dir_config.UPLOAD_MEDIA_DIR)
-        self.UPLOAD_AUDIO_URL = Path(
-            os.path.sep
-        ) / dir_config.UPLOAD_AUDIO_DIR.relative_to(dir_config.UPLOAD_MEDIA_DIR)
-        self.UPLOAD_VIDEO_URL = Path(
-            os.path.sep
-        ) / dir_config.UPLOAD_VIDEO_DIR.relative_to(dir_config.UPLOAD_MEDIA_DIR)
-        self.UPLOAD_FILE_URL = Path(
-            os.path.sep
-        ) / dir_config.UPLOAD_FILE_DIR.relative_to(dir_config.UPLOAD_DIR)
-
-        self.SQL_DATABASE_URL = Path(
-            os.path.sep
-        ) / dir_config.SQL_DATABASE_DIR.relative_to(dir_config.ROOT_DIR)
-
-    def __iter__(self):
-        for item in self.__dict__:
-            if "URL" in item:
-                yield self.__dict__[item]
-
-    def __next__(self):
-        return next(self)
-
-    def dict(self):
-        return {item: self.__dict__[item] for item in self.__dict__ if "URL" in item}
-
-    def __str__(self):
-        return str(list(self))
 
 
-class DatabaseFile(object):
+class Dir(DirType):
     def __init__(self, dir_config: DirConfig):
-        self.dir: Path = dir_config.DATABASE_DIR
-        self.sql: Path = dir_config.SQL_DATABASE_DIR
-        self.nosql: Path = dir_config.NOSQL_DATABASE_DIR
+        ...
 
 
-class StaticFile(object):
+class DatabaseDir(Dir):
+    DATABASE_PATH = "database"
+    SQL_PATH = "sql"
+    NOSQL_PATH = "no_sql"
+
     def __init__(self, dir_config: DirConfig):
-        self.dir: Path = dir_config.STATIC_DIR
-        self.images: Path = dir_config.STATIC_IMAGE_DIR
-        self.audios: Path = dir_config.STATIC_AUDIO_DIR
-        self.videos: Path = dir_config.STATIC_VIDEO_DIR
-        self.files: Path = dir_config.STATIC_FILE_DIR
-        self.css: Path = dir_config.STATIC_CSS_DIR
-        self.js: Path = dir_config.STATIC_JS_DIR
-        self.fonts: Path = dir_config.STATIC_FONT_DIR
-        self.medias: Path = dir_config.STATIC_MEDIA_DIR
+        self.DATABASE_DIR = dir_config.BASE_DIR / self.DATABASE_PATH
+        self.SQL_DIR = self.DATABASE_DIR / self.SQL_PATH
+        self.NOSQL_DIR = self.DATABASE_DIR / self.NOSQL_PATH
+
+        self.dir: Path = self.DATABASE_DIR
+        self.sql: Path = self.SQL_DIR
+        self.nosql: Path = self.NOSQL_DIR
 
 
-class UploadedFile(object):
+class StaticDir(Dir):
+    STATIC_PATH = "static"
+    MEDIA_PATH = "media"
+    FILE_PATH = "file"
+    CSS_PATH = "css"
+    JS_PATH = "js"
+    FONT_PATH = "font"
+    IMAGE_PATH = "image"
+    AUDIO_PATH = "audio"
+    VIDEO_PATH = "video"
+
     def __init__(self, dir_config: DirConfig):
-        self.dir: Path = dir_config.UPLOAD_DIR
-        self.images: Path = dir_config.UPLOAD_IMAGE_DIR
-        self.audios: Path = dir_config.UPLOAD_AUDIO_DIR
-        self.videos: Path = dir_config.UPLOAD_VIDEO_DIR
-        self.files: Path = dir_config.UPLOAD_FILE_DIR
-        self.medias: Path = dir_config.UPLOAD_MEDIA_DIR
+        self.STATIC_DIR = dir_config.BASE_DIR / self.STATIC_PATH
+
+        self.MEDIA_DIR = self.STATIC_DIR / self.MEDIA_PATH
+        self.FILE_DIR = self.STATIC_DIR / self.FILE_PATH
+
+        self.IMAGE_DIR = self.MEDIA_DIR / self.IMAGE_PATH
+        self.AUDIO_DIR = self.MEDIA_DIR / self.AUDIO_PATH
+        self.VIDEO_DIR = self.MEDIA_DIR / self.VIDEO_PATH
+
+        self.CSS_DIR = self.FILE_DIR / self.CSS_PATH
+        self.JS_DIR = self.FILE_DIR / self.JS_PATH
+        self.FONT_DIR = self.FILE_DIR / self.FONT_PATH
+
+        self.dir: Path = self.STATIC_DIR
+        self.medias: Path = self.MEDIA_DIR
+        self.files: Path = self.FILE_DIR
+
+        self.images: Path = self.IMAGE_DIR
+        self.audios: Path = self.AUDIO_DIR
+        self.videos: Path = self.VIDEO_DIR
+        self.css: Path = self.CSS_DIR
+        self.js: Path = self.JS_DIR
+        self.fonts: Path = self.FONT_DIR
 
 
-class TemplateFile(object):
+class UploadDir(Dir):
+    UPLOAD_PATH = "upload"
+    MEDIA_PATH = "media"
+    FILE_PATH = "file"
+    IMAGE_PATH = "image"
+    AUDIO_PATH = "audio"
+    VIDEO_PATH = "video"
+
     def __init__(self, dir_config: DirConfig):
-        self.dir: Path = dir_config.TEMPLATE_DIR
+        self.UPLOAD_DIR = dir_config.BASE_DIR / self.UPLOAD_PATH
+
+        self.MEDIA_DIR = self.UPLOAD_DIR / self.MEDIA_PATH
+        self.FILE_DIR = self.UPLOAD_DIR / self.FILE_PATH
+
+        self.IMAGE_DIR = self.MEDIA_DIR / self.IMAGE_PATH
+        self.AUDIO_DIR = self.MEDIA_DIR / self.AUDIO_PATH
+        self.VIDEO_DIR = self.MEDIA_DIR / self.VIDEO_PATH
+
+        self.dir: Path = self.UPLOAD_DIR
+        self.medias: Path = self.MEDIA_DIR
+        self.files: Path = self.FILE_DIR
+
+        self.images: Path = self.IMAGE_DIR
+        self.audios: Path = self.AUDIO_DIR
+        self.videos: Path = self.VIDEO_DIR
+
+
+class TemplateDir(Dir):
+    TEMPLATE_PATH = "templates"
+
+    def __init__(self, dir_config: DirConfig):
+        self.TEMPLATE_DIR = dir_config.BASE_DIR / self.TEMPLATE_PATH
+        self.dir: Path = self.TEMPLATE_DIR
+
+
+class CacheDir(Dir):
+    CACHE_PATH = "cache"
+
+    def __init__(self, dir_config: DirConfig):
+        self.CACHE_DIR = dir_config.BASE_DIR / self.CACHE_PATH
+        self.dir = self.CACHE_DIR
 
 
 class WebAssets(DirConfig):
-    files: FileSettings
-    url: UrlConfig
-    template: TemplateFile
-    upload: UploadedFile
-    static: StaticFile
-    database: DatabaseFile
+    template: Dir
+    upload: Dir
+    static: Dir
+    database: Dir
+    cache: Dir
 
-    def __init__(self, file_settings: FileSettings):
-        super().__init__(file_settings=file_settings)
+    def __init__(self, base_dir, sub_dir=None, *dir_types: T.Type[Dir], dry_run=True):
+        super().__init__(base_dir=base_dir, sub_dir=sub_dir or "")
         # basic settings
-        self.dir = self.ROOT_DIR
-        # url settings
-        self.url = UrlConfig(dir_config=self)
+        self.dir = self.BASE_DIR
 
-        # files
-        self.template = TemplateFile(dir_config=self)
-        self.upload = UploadedFile(dir_config=self)
-        self.static = StaticFile(dir_config=self)
-        self.database = DatabaseFile(dir_config=self)
-        files_created: list[T.Union[str, Path]] = make_paths(self)
-        file_logger = logging.getLogger(str(self.dir.parent.name))
-        logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-        if files_created:
-            file_logger.info(f"(DIR):{self.dir.parent}")
-            for file in sorted(files_created):
+        self.dir_logger = logging.getLogger(str(self.dir.parent.name))
+        self.dir_logger.setLevel(logging.INFO)
+        handler = logging.StreamHandler(stream=sys.stdout)
+        self.dir_logger.addHandler(handler)
+
+        # directories
+        self.template: TemplateDir = TemplateDir(dir_config=self)
+        self.upload: UploadDir = UploadDir(dir_config=self)
+        self.static: StaticDir = StaticDir(dir_config=self)
+        self.database: DatabaseDir = DatabaseDir(dir_config=self)
+        self.cache: CacheDir = CacheDir(dir_config=self)
+        directories = [
+            *self,
+            *self.template,
+            *self.upload,
+            *self.static,
+            *self.database,
+            *self.cache,
+        ]
+        for dir_type in dir_types:
+            directories.extend(*dir_type(self))
+        if dry_run:
+            self.dir_logger.info("== Following directories would be created ==")
+            for dir_name in directories:
+                self.dir_logger.info(pformat(dir_name))
+        else:
+            self.make_dirs(directories)
+
+    def make_dirs(self, directories):
+        dirs_created: list[T.Union[str, Path]] = make_paths(directories)
+
+        if dirs_created:
+            self.dir_logger.info(f"(DIR):{self.dir.parent}")
+            for file in sorted(dirs_created):
                 if file:
-                    file_logger.info(Path(file).relative_to(self.dir.parent))
+                    self.dir_logger.info(Path(file).relative_to(self.dir.parent))
 
     def __dir__(self) -> T.Iterable[str]:
         return sorted(
