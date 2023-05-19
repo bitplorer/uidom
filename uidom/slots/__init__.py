0d6f775f1ab7b9e3d330f9d5f52d112c79845eb8
bitplorer <bitplorer@outlook.com>
2023-05-19 14:06:02 +0530

    imported: slots module

--- uidom/slots/__init__.py
+++ uidom/slots/__init__.py
@@ -1,8 +1,9 @@
 # Copyright (c) 2022 uidom
-# 
+#
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
 
-from .custom_element_slot import * # isort: skip
+from .custom_element_slot import *  # isort: skip
 from .web_component_slot import *  # isort: skip
+from .slots import *  # isort: skip
