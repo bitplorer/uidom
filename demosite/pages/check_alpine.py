4f536473a7958aceb13caf3dfc1896a8139885a7
bitplorer <bitplorer@outlook.com>
2023-07-07 12:31:44 +0530

    demosite: wip

--- demosite/pages/check_alpine.py
+++ demosite/pages/check_alpine.py
@@ -3,10 +3,83 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-from dataclasses import dataclass, field, make_dataclass
 
 from demosite.api import api
+from demosite.document import document
+from uidom.alpinejs.dataset import DataSet, dataset_pressed, dataset_ripple
 from uidom.dom import *
+
+
+def data_ripple(className="", **kwargs):
+    with DataSet() as _ripple:
+        dataset_ripple()
+        dataset_pressed()
+        attr(
+            className="""
+            relative w-24 h-10 rounded border border-gray-300 transform transition
+            [&>[data-ripple=true]]:absolute
+            [&>[data-ripple=true]]:rounded-full
+            [&>[data-ripple=true]]:pointer-events-none
+            [&>[data-ripple=true]]:bg-rose-400
+            [&>[data-ripple=true]]:animate-pulse
+            %s
+            """
+            % className,
+            **kwargs,
+        )
+    return _ripple
+
+
+class Button(Component):
+    def render(self, *args, className="", asElement=None, **kwargs):
+        with data_ripple(
+            className=f"overflow-hidden m-2 p-2 {className}",
+            **kwargs,
+        ) as ripple_btn:
+            button(*args) if asElement is None else asElement(*args)
+        return ripple_btn
+
+
+@api.get("/ripple")
+def ripp():
+    with document() as doc:
+        Button(
+            "Hello",
+            # x_bind_data_ripple_light="true",
+            # asElement=a,
+            className="""
+            bg-rose-500 text-pink-100 items-center 
+            text-center rounded-lg bg-opacity-80 font-oswald 
+            leading-tight""",
+        )
+    return doc
+
+
+class side_menu(Component):
+    def render(self, menus=[]):
+        with div(className="flex") as side:
+            with div(className="w-1/4 min-h-screen bg-rose-600"):
+                comment("Side Menu Content")
+                with ul(className="py-4"):
+                    for i, name in enumerate(menus):
+                        if i == 0:
+                            with li(
+                                className="mb-2 px-4 py-2 bg-white text-gray-800 mt-2"
+                            ):
+                                a(name, href="#", className="block")
+                        else:
+                            with li(className="mb-2 px-4 py-2 bg-white text-gray-800"):
+                                a(name, href="#", className="block")
+
+            with div(className="w-3/4 min-h-screen bg-gray-100"):
+                comment("Main Content")
+                h1("Main Content", className="text-2xl font-bold mt-8 ml-8")
+        return side
+
+
+@api.get("/side_menu")
+def side():
+    return document(side_menu(menus=["Home", "About", "Services", "Contact"]))
 
 
 class chartjs(Component):
@@ -77,3 +150,7 @@ window.dispatchEvent(event);
             
             """
         )
+
+
+if __name__ == "__main__":
+    print(side_menu())
