2b3abf691d9732312057f34488ce29626855e764
bitplorer <bitplorer@outlook.com>
2023-09-28 23:55:49 +0530

    working example of DataSet usage

--- uidom/alpinejs/ripple_btn.py
+++ uidom/alpinejs/ripple_btn.py
@@ -3,22 +3,21 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-from uidom.alpinejs import DataSet, dataset_pressed, dataset_ripple
+from uidom.alpinejs import DataSet, dataset_ripple  # , dataset_pressed
 from uidom.dom import *
 
 
-def data_ripple(className="", **kwargs):
+def data_ripple(className="[&>[data-ripple=true]]:bg-white/30", **kwargs):
     with DataSet() as _ripple:
         dataset_ripple()
-        dataset_pressed()
+        # dataset_pressed()
         attr(
             className="""
-            relative w-24 h-10 rounded
+            relative overflow-hidden
             [&>[data-ripple=true]]:absolute
             [&>[data-ripple=true]]:rounded-full
             [&>[data-ripple=true]]:pointer-events-none
-            [&>[data-ripple=true]]:bg-rose-400
-            [&>[data-ripple=true]]:animate-pulse
+            [&>[data-ripple=true]]:animate-ripple
             %s
             """
             % className,
@@ -28,10 +27,17 @@ def data_ripple(className="", **kwargs):
 
 
 class Button(Component):
-    def render(self, *args, className="", asElement=None, **kwargs):
-        with data_ripple(
-            className=f"overflow-hidden m-2 p-2 {className}",
-            **kwargs,
-        ) as ripple_btn:
-            button(*args) if asElement is None else asElement(*args)
-        return ripple_btn
+    def render(self, *args, _as=button, **kwargs):
+        with data_ripple(className="[&>[data-ripple=true]]:bg-stone-400/40"):
+            return _as(*args, **kwargs)
+
+
+if __name__ == "__main__":
+    print(
+        Button(
+            "Login",
+            className="flex px-4 py-2 bg-rose-400 [data-loading=true]:animate-pulse",
+            x_on_mouseup="data-loading=true",
+            x_data="{'data-loading': false}",
+        )
+    )
