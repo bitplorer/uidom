91a82563a3d3192dcb6dcb2eb9a2c402dd83d31b
bitplorer <bitplorer@outlook.com>
2023-10-02 17:39:35 +0530

    deleted files

--- demosite/app/route.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2023 UiDOM
-#
-# This software is released under the MIT License.
-# https://opensource.org/licenses/MIT
-
-
-def get():
-    return "My New App"
--- demosite/sellers/{dashboard}/route.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2023 UiDOM
-#
-# This software is released under the MIT License.
-# https://opensource.org/licenses/MIT
-
-
-def get(id: str):
-    return f"dashboard {id}"
